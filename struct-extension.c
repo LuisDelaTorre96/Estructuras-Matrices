9f835b180c14965f8003a6af85cefd6241c6237f
LuisDelaTorre96 <al24760968@ite.edu.mx>
2025-11-20 20:56:16 -0800

    Avance de ejercicio, faltan corregir cosas para que funcione correctamente

--- struct-extension.c
+++ struct-extension.c
@@ -31,7 +31,7 @@ struct Estudiante{
 };
 
 int main(){
-	struct Estudiante datos[MAX_ESTUDIANTES];
+	struct Estudiante datos[MAX_ESTUDIANTES]; // Maximo 100 estudiantes
 	int numeroEstudiantes = 0;
 	int opcion;
 
@@ -48,7 +48,7 @@ int main(){
 		scanf("%d", &opcion);
 
 		switch(opcion){
-			case 1:
+			case 1: // Capturar estudiantes
 				int cantidad;
 
 				printf("\nIngrese el numero de estudiantes a capturar (maximo 100 estudiantes): ");
@@ -58,8 +58,8 @@ int main(){
 
 				if(total > MAX_ESTUDIANTES){
 					printf("\nError: excede el maximo de estudiantes a capturar (%d)", MAX_ESTUDIANTES);
-				}else{
-					printf("\nIngrese los datos de los estudiantes.\n");
+				} else{
+					printf("\nIngrese los datos de los estudiantes\n");
 					for(int i = numeroEstudiantes; i < total; i++){
 						float suma = 0.0f;
 
@@ -68,20 +68,21 @@ int main(){
 						printf("Matricula: ");
 						scanf("%d", &datos[i].matricula);
 
-						getchar();
+						getchar(); // Elimina el /n del scanf() anterior antes de leer el nombre con fgets()
 
 						printf("Nombre: ");
 						fgets(datos[i].nombre, 50, stdin);
 
-						datos[i].nombre[strcspn(datos[i].nombre, "\n")] = '\0'; // Elimina el \n de fgets().
+						datos[i].nombre[strcspn(datos[i].nombre, "\n")] = '\0'; // Elimina el \n de fgets()
 
 						printf("Edad: ");
 						scanf("%d", &datos[i].edad);
 
 						for(int j = 0; j < MAX_MATERIAS; j++){
-							printf("\nMateria %d\n", j + 1);
+							printf("Materia %d\n", j + 1);
 
 							getchar();
+
 							printf("Nombre de la materia: ");
 							fgets(datos[i].materias[j].nombre, 30, stdin);
 							datos[i].materias[j].nombre[strcspn(datos[i].materias[j].nombre, "\n")] = '\0';
@@ -97,30 +98,59 @@ int main(){
 					}
 					numeroEstudiantes = total;
 				}
-				break;
-			case 2:
-				printf("\n");
+				printf("\n---- RECUERDA GUARDAR LOS ESTUDIANTES CAPTURADOS ----\n");
 
 				break;
-			case 3:
+
+			case 2: // Guardar datos capturados en archivo.txt
 				if(numeroEstudiantes == 0){
-					printf("\nNo hay estudiantes capturados.\n");
+					printf("\nNo hay estudiantes capturados para guardar.\n");
 				} else{
-					printf("\n--- Lista de estudiantes capturados ---\n");
-					for(int i = 0; i < numeroEstudiantes; i ++){
+					FILE *f = fopen("estudiantes.txt", "w");
+
+					fprintf(f, "Listado de estudiantes\n");
+
+					for(int i = 0; i < numeroEstudiantes; i++){
+						fprintf(f, "\nEstudiante %d\n", i + 1);
+						fprintf(f, "%d\n", datos[i].matricula);
+						fprintf(f, "%s\n", datos[i].nombre);
+						fprintf(f, "%d\n", datos[i].edad);
+						for(int j = 0; j < MAX_MATERIAS; j++){
+							fprintf(f, "%s\n", datos[i].materias[j].nombre);
+							fprintf(f, "%.2f\n", datos[i].materias[j].calificacion);
+						}
+						fprintf(f, "%.2f\n", datos[i].promedio);
+					}
+
+					fclose(f);
+					printf("\nDatos guardados en 'Estudiantes.txt'.\n");
+				}
+
+				break;
+
+			case 3: // Cargar datos desde archivo.txt
+				FILE f* = fopen("Estudiantes.txt", "r");
+
+				if(numeroEstudiantes == 0){
+					printf("No hay estudiantes guardados para cargar.\n");
+				} else{
+					printf("Mostrando estudiantes capturados desde 'Estudiantes.txt'\n");
+					for(int i = 0; i < numeroEstudiantes; i++){
 						printf("\nEstudiante %d\n", i + 1);
 						printf("Matricula: %d\n", datos[i].matricula);
 						printf("Nombre: %s\n", datos[i].nombre);
 						printf("Edad: %d\n", datos[i].edad);
 						printf("Materias:\n");
 						for(int j = 0; j < MAX_MATERIAS; j++){
-							printf(" %d. %s - Calificacion: %.2f\n", j + 1, datos[i].materias[j].nombre, datos[i].materias[j].calificacion);
+							printf(" %d. %s | Calificacion: %.2f\n", j + 1, datos[i].materias[j].nombre, datos[i].materias[j].calificacion);
 						}
-						printf("Promedio: %.2f", datos[i].promedio);
+						printf("Promedio: %.2f\n", datos[i].promedio);
 					}
 				}
+
 				break;
-			case 4:
+
+			case 4: // Buscar estudiante por matricula
 				if(numeroEstudiantes == 0){
 					printf("\nNo hay estudiantes capturados.\n");
 				} else{
@@ -153,10 +183,51 @@ int main(){
 					}
 				}
 				break;
-			case 5:
-				printf("\n");
 
-				break;
+			case 5: // Consultar promedios
+				if(numeroEstudiantes == 0){
+					printf("\nNo hay estudiantes capturados aun.\n");
+				}else{
+					int opcion2;
+					printf("\n--- Consultar promedios ---\n");
+					printf("1. Consultar el mejor promedio.");
+					printf("2. Consultar el promedio general de los estudiantes capturados.\n");
+					printf("Elija una opcion: ");
+					scanf("%d", &opcion2);
+
+					switch(opcion2){
+						case 1: // Consultar el mejor promedio
+							int indiceMayor = 0;
+							float mejorPromedio = datos[0].promedio;
+
+							for(int i = 0; i < numeroEstudiantes; i++){
+								if(datos[i].promedio > mejorPromedio){
+									mejorPromedio = datos[i].promedio;
+									indiceMayor = i;
+								}
+							}
+
+							printf("\nEstudiante con mejor promedio:\n");
+							printf("Matricula: %d\n", datos[indiceMayor].matricula);
+							printf("Nombre: %s\n", datos[indiceMayor].nombre);
+							printf("Promedio: %.2f", datos[indiceMayor].promedio);
+
+							break;
+						case 2: // Consultar el promedio general de los estudiantes capturados
+							float sumaPromedios = 0.0;
+							float promedioGeneral = 0.0;
+
+							for(int i = 0; i < numeroEstudiantes; i++){
+								sumaPromedios += datos[i].promedio;
+							}
+
+							promedioGeneral = sumaPromedios / numeroEstudiantes;
+
+							printf("\nEl promedio general de los %d estudiantes capturados es %.2f", numeroEstudiantes, promedioGeneral);
+
+							break;
+					}
+					break;
 			case 6:
 				printf("\nSaliendo del programa...\n");
 
